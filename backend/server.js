b1029b535f5bac4477520f3dd762ff3f623d7dbd
Vraj Prajapati <vrajprajapati28506@gmail.com>
2025-04-06 22:14:00 +0530

    Update server to listen on all interfaces (0.0.0.0) for Docker compatibility

--- backend/server.js
+++ backend/server.js
@@ -98,6 +98,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
